d81636b62ebb165259e94e6e1ff6901fcb621f3d
Eric <Karstenswobi@gmail.com>
2024-04-08 12:12:51 +0200

    adding API

--- src/ColorCard.js
+++ src/ColorCard.js
@@ -1,11 +1,29 @@
-import React from "react";
-import "./ColorCard.css";
+import React, { useState, useEffect } from "react";
+import useLocalStorageState from "use-local-storage-state";
 
 const ColorCard = ({ color }) => {
+  const [colorName, setColorName] = useState("");
+
+  useEffect(() => {
+    const fetchColorName = async () => {
+      try {
+        const response = await fetch(
+          `https://www.thecolorapi.com/id?hex=${color.value.substr(1)}`
+        );
+        const data = await response.json();
+        setColorName(data.name.value);
+      } catch (error) {
+        console.error("Error fetching color name:", error);
+      }
+    };
+
+    fetchColorName();
+  }, [color.value]);
+
   return (
     <div className="color-card" style={{ backgroundColor: color.value }}>
       <div className="color-info">
-        <p className="color-name">{color.name}</p>
+        <p className="color-name">{colorName}</p>
         <p className="color-hex">{color.value}</p>
         <p className="color-role">{color.role}</p>
       </div>
